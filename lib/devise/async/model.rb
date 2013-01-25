d02dbef6b1032b029a7b0ed2ea3e4ed0effff149
Mohamed Magdy <eng.mohamagdy@gmail.com>
2013-01-25 13:56:21 +0200

    Returning the super method if the enabled option was disabled

--- lib/devise/async/model.rb
+++ lib/devise/async/model.rb
@@ -36,18 +36,16 @@ module Devise
       # processing instead of sending it inline as devise does by
       # default.
       def send_devise_notification(notification, opts = {})
-        if Devise::Async.enabled
-          # If the record is dirty we keep pending notifications to be enqueued
-          # by the callback and avoid before commit job processing.
-          if changed?
-            devise_pending_notifications << [ notification, opts ]
-          # If the record isn't dirty (aka has already been saved) enqueue right away
-          # because the callback has already been triggered.
-          else
-            Devise::Async::Worker.enqueue(notification, self.class.name, self.id.to_s, opts)
-          end
+        return super unless Devise::Async.enabled
+        
+        # If the record is dirty we keep pending notifications to be enqueued
+        # by the callback and avoid before commit job processing.
+        if changed?
+          devise_pending_notifications << [ notification, opts ]
+        # If the record isn't dirty (aka has already been saved) enqueue right away
+        # because the callback has already been triggered.
         else
-          super # Calling the devise's original method
+          Devise::Async::Worker.enqueue(notification, self.class.name, self.id.to_s, opts)
         end
       end
 
