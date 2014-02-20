9ded1de5e31e844e22e626ceae62271aadd77eba
Ken Mayer <pair+ken@pivotallabs.com>
2014-02-20 11:51:18 -0800

    Add more tests

--- test/devise/async/backend/sucker_punch_test.rb
+++ test/devise/async/backend/sucker_punch_test.rb
@@ -4,10 +4,21 @@ module Devise
   module Async
     module Backend
       describe "SuckerPunch" do
+=begin
+        it "enqueus the job" do
+          sucker_punch_instance = mock()
+          sucker_punch_instance.expects(:perform).once.with(:mailer_method, "User", 123, {})
+          SuckerPunch.any_instance.expects(:async).once.returns(sucker_punch_instance)
+
+          SuckerPunch.enqueue(:mailer_method, "User", 123, {})
+        end
+=end
         it "delegates to devise mailer when delivering" do
           user = create_user
           ActionMailer::Base.deliveries = []
-          Backend::SuckerPunch.new.perform(:confirmation_instructions, "User", user.id, {})
+
+          SuckerPunch.new.perform(:confirmation_instructions, "User", user.id, {})
+
           ActionMailer::Base.deliveries.size.must_equal 1
         end
       end
