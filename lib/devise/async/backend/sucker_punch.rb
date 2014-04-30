00d8956263124949673511a301fd7cc48d2a81cc
Ken Mayer <pair+ken@pactapp.com>
2014-04-29 21:35:07 -0700

    Fix comments for connection pooling

--- lib/devise/async/backend/sucker_punch.rb
+++ lib/devise/async/backend/sucker_punch.rb
@@ -10,15 +10,11 @@ module Devise
           new.async.perform(*args)
         end
 
-        # Loads the resource record and sends the email.
-        #
-        # It uses `orm_adapter` API to fetch the record in order to enforce
-        # compatibility among diferent ORMs.
+        # Return the connection to the pool after we're done with it
+        # see: https://github.com/brandonhilkert/sucker_punch#usage
         def perform(method, resource_class, resource_id, *args)
           ActiveRecord::Base.connection_pool.with_connection do
-            resource = resource_class.constantize.to_adapter.get!(resource_id)
-            args[-1] = args.last.symbolize_keys if args.last.is_a?(Hash)
-            mailer_class(resource).send(method, resource, *args).deliver
+            super
           end
         end
 
