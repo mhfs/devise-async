bff2e955bd8db677c5982be0316321d1cea47052
Sebastian Oelke <soelke@babbel.com>
2014-12-01 10:48:37 +0100

    Adds remembering of the locale before an asynchronous task is run. Sets the remembered locale when an asynchronous task is run.

--- lib/devise/async/backend/base.rb
+++ lib/devise/async/backend/base.rb
@@ -10,16 +10,26 @@ module Devise
         #
         # It uses `orm_adapter` API to fetch the record in order to enforce
         # compatibility among diferent ORMs.
+        #
+        # This method is executed within the scope of the locale of
+        # the calling thread.
         def perform(method, resource_class, resource_id, *args)
-          resource = resource_class.constantize.to_adapter.get!(resource_id)
-          args[-1] = args.last.symbolize_keys if args.last.is_a?(Hash)
-          mailer_class(resource).send(method, resource, *args).deliver
+          I18n.with_locale locale_from_args(args) do
+            resource = resource_class.constantize.to_adapter.get!(resource_id)
+            args[-1] = args.last.symbolize_keys if args.last.is_a?(Hash)
+            mailer_class(resource).send(method, resource, *args).deliver
+          end
         end
 
         private
 
         def mailer_class(resource = nil)
           @mailer_class ||= resource.try(:devise_mailer) || Devise.mailer
+        end
+
+        def locale_from_args(args)
+          args_last = args.last
+          args_last.delete('locale') if args_last.is_a?(Hash)
         end
       end
     end
--- test/devise/async/model_test.rb
+++ test/devise/async/model_test.rb
@@ -71,6 +71,30 @@ module Devise
         admin.send(:devise_pending_notifications).must_equal []
         Worker.expects(:enqueue).never
       end
+
+      describe "custom locale set" do
+
+        # Set a custom locale
+        before { I18n.locale = :de }
+
+        # locale == nil indicates the usage of the default_locale
+        after { I18n.locale = nil }
+
+        it "should set the current locale to the args" do
+          admin = create_admin
+          Worker.expects(:enqueue).with(:confirmation_instructions, "Admin", admin.id.to_s, instance_of(String), { 'locale' => :de })
+          admin.send_confirmation_instructions
+        end
+      end
+
+      describe "default locale set" do
+
+        it "should not set the current locale to the args" do
+          admin = create_admin
+          Worker.expects(:enqueue).with(:confirmation_instructions, "Admin", admin.id.to_s, instance_of(String), {})
+          admin.send_confirmation_instructions
+        end
+      end
     end
   end
 end
